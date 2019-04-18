687bae78892b67f8519a841a13e008efab59ec99
Rajnish Kumar <rajnish.kumar@rotary.org>
2019-04-19 00:04:15 +0530

    Initial Commit

---
+++ src/client/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from './Components/Header';
+//import Content from './Components/Content';
+//import Footer from './Components/Footer';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
